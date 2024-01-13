c7de5abe595c59023bb21ff69500b1bcea1b3a84
nhoeger <91960250+nhoeger@users.noreply.github.com>
2024-01-13 16:49:49 +0100

    Update Setup_Skript.bash

--- bgpsim/other/Setup_Skript.bash
+++ bgpsim/other/Setup_Skript.bash
@@ -7,12 +7,12 @@ export PATH="$PATH:/home/$USER/.local/bin"
 apt install git -y
 #Replace git clone with link to your repository
 #Creation of Personal acces token necessary to clone repository by command line
-#git clone #Link to repository
+git clone https://github.com/nhoeger/bgpsim
 #Replace with Name of your repository you cloned to cd inside there
-#cd # directory name of cloned repository
+cd bgpsim 
 #git checkout if neccessary as you need to change the branch of the repository
-git checkout #Name of branch
-git pull
+#git checkout #Name of branch
+#git pull
 pipenv run pip3 list
 pipenv run pip3 install click
 pipenv run pip3 install networkx
