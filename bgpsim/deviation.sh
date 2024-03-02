42e58f341a45051de0d1acedbf867b4e408ca724
nhoeger <nils@l-hoeger.com>
2024-03-02 16:08:26 +0100

    Updated deviation.sh

--- bgpsim/deviation.sh
+++ bgpsim/deviation.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#for ((i=1; i<=2; i++)); do
-#  pipenv run python -m bgpsecsim generate --trials 1 deviation_figure caida-data/20221101.as-rel.txt outputs/figure_one_down_only_100trials | tee down_only_one250.txt
-#done
-pipenv run python -m bgpsecsim generate --trials 1 deviation_figure caida-data/20221101.as-rel.txt outputs/figure_one_down_only_100trials | tee down_only_one250.txt
-pipenv run python -m bgpsecsim generate --trials 10 deviation_figure caida-data/20221101.as-rel.txt outputs/figure_one_down_only_100trials | tee down_only_one250.txt
-pipenv run python -m bgpsecsim generate --trials 100 deviation_figure caida-data/20221101.as-rel.txt outputs/figure_one_down_only_100trials | tee down_only_one250.txt
-pipenv run python -m bgpsecsim generate --trials 1000 deviation_figure caida-data/20221101.as-rel.txt outputs/figure_one_down_only_100trials | tee down_only_one250.txt
-pipenv run python -m bgpsecsim generate --trials 10000 deviation_figure caida-data/20221101.as-rel.txt outputs/figure_one_down_only_100trials | tee down_only_one250.txt
+for ((i=1; i<=10000; i *= 10)); do
+  pipenv run python -m bgpsecsim generate --trials "$i" deviation_figure caida-data/20221101.as-rel.txt outputs/figure_one_down_only_100trials | tee deviation.txt
+done
