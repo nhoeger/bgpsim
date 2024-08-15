8079f4ab13aabd6d26223d7213259defbe91d854
nhoeger <nils@l-hoeger.com>
2024-08-15 16:09:28 +0200

    updated skript

--- bgpsim/skript_run-all-graphs.sh
+++ bgpsim/skript_run-all-graphs.sh
@@ -2,5 +2,7 @@
 
 # More trials and steps provide more precise results, but increase running time
 number_of_trials=1000
-# pipenv run python -m bgpsecsim generate --trials "$number_of_trials" otc_figures caida-data/20240501.as-rel.txt outputs/figure_roles_1_"$number_of_trials"trials
-pipenv run python -m bgpsecsim generate --trials "$number_of_trials" improved_performance_test caida-data/20240501.as-rel.txt outputs/figure_roles_1_"$number_of_trials"trials | tee  reduced.txt
+
+# Comment out test one wants to run
+# pipenv run python -m bgpsecsim generate --trials "$number_of_trials" otc_figures caida-data/20240501.as-rel.txt outputs/figure_roles_1_"$number_of_trials"trials | tee  deviation.txt
+# pipenv run python -m bgpsecsim generate --trials "$number_of_trials" improved_performance_test caida-data/20240501.as-rel.txt outputs/figure_roles_1_"$number_of_trials"trials | tee  reduced.txt
